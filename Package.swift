ba39afad13b1ed4968540f84daccacf46cc1cb2b
Alex Lucas <alucas96@me.com>
2020-03-07 11:13:25 -0500

    misc cleanup and trimming

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-slider",
+    name: "RoundSlider",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -21,8 +21,6 @@ let package = Package(
         .target(
             name: "RoundSlider",
             dependencies: []),
-        .testTarget(
-            name: "swift-sliderTests",
-            dependencies: ["swift-slider"]),
+       
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import swift_sliderTests
-
-var tests = [XCTestCaseEntry]()
-tests += swift_sliderTests.allTests()
-XCTMain(tests)
