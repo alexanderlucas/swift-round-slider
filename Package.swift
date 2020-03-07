0217d4cace66703489af8ec370f5082cfa350b69
Alex Lucas <alucas96@me.com>
2020-03-07 10:36:47 -0500

    slider initial

--- Package.swift
+++ Package.swift
@@ -10,6 +10,9 @@ let package = Package(
         .library(
             name: "swift-slider",
             targets: ["swift-slider"]),
+        .library(
+        name: "RoundSlider",
+        targets: ["RoundSlider"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,6 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "swift-slider",
+            dependencies: []),
+        .target(
+            name: "RoundSlider",
             dependencies: []),
         .testTarget(
             name: "swift-sliderTests",
---
+++ Sources/RoundSlider/RoundSlider.swift
@@ -0,0 +1,172 @@
+//
+//  SwiftSlider.swift
+//  Good Slider
+//
+//  Created by Alex Lucas on 10/18/19.
+//  Copyright © 2019 Alex Lucas. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, macOS 10.15, *)
+class SliderData: ObservableObject {
+    
+    var minValue: Double
+    var maxValue: Double
+    var defaultValue: Double
+    var color: Color
+    
+    init(minValue: Double, maxValue: Double, defaultValue: Double, color: Color) {
+        self.minValue = minValue
+        self.maxValue = maxValue
+        self.defaultValue = defaultValue
+        self.color = color
+        
+        self.value = defaultValue
+    }
+
+    @Published var value: Double
+}
+
+@available(iOS 13.0, macOS 10.15, *)
+struct RoundSlider: View {
+    @EnvironmentObject var data: SliderData
+    @GestureState var isDetectingLongPress = false
+    @State var moving: Bool = false
+    @State var initialPosition: CGSize = .zero
+    
+    var valueString: String {
+        String(Int(data.value))
+    }
+    
+       
+
+    var body: some View {
+        
+        return
+            GeometryReader { (geometry) -> AnyView in
+                var max: CGFloat {
+                    (geometry.size.width / 2 - 26)
+                }
+                var min: CGFloat {
+                    (geometry.size.width / -2 + 26)
+                }
+                let press = LongPressGesture(minimumDuration: 1)
+                    .updating(self.$isDetectingLongPress) { currentState, gestureState, transaction in
+                        
+                        gestureState = currentState
+                    }
+
+                let drag = DragGesture()
+                    .onChanged {
+                        var tran = $0.translation
+
+                        tran.width += self.initialPosition.width
+                        if tran.width < min {
+                            tran.width = min
+                        }
+                        if tran.width > max {
+                            tran.width = max
+                        }
+                        
+                        self.moving = true
+
+                        
+                        let newDiff = CGFloat(self.data.maxValue - self.data.minValue)
+                        let oldDiff = max - min
+                        
+                        let valDiff = tran.width - max
+                        
+                        let newVal = (newDiff / oldDiff) * valDiff + CGFloat(self.data.maxValue)
+                        
+                        self.data.value = Double(newVal)
+                        
+                }
+                .onEnded {
+                    var tran = $0.translation
+                    
+                    tran.width += self.initialPosition.width
+
+                    if tran.width < min {
+                        tran.width = min
+                    }
+                    if tran.width > max {
+                        tran.width = max
+                    }
+
+                    self.initialPosition.width = tran.width
+                    self.initialPosition.height = 0
+                    self.moving = false
+
+                    let newDiff = CGFloat(self.data.maxValue - self.data.minValue)
+                    let oldDiff = max - min
+                    
+                    let valDiff = tran.width - max
+                    
+                    let newVal = (newDiff / oldDiff) * valDiff + CGFloat(self.data.maxValue)
+                    
+                    self.data.value = Double(newVal)
+                    
+
+                }
+                
+                var offsetWidth: CGFloat {
+                    let oldDiff = max - min
+                    let newDiff = CGFloat(self.data.maxValue - self.data.minValue)
+                    let valDiff = CGFloat(self.data.value - self.data.maxValue)
+                    
+                    return (oldDiff / newDiff) * valDiff + max
+                }
+                
+                return AnyView(
+                    ZStack {
+                        Rectangle()
+                            .frame(width: geometry.size.width, height: 40)
+                            .cornerRadius(20)
+                            .animation(.spring())
+                            .foregroundColor(.red)
+                        Thumb(label: self.valueString, isUp: self.moving || self.isDetectingLongPress, color: self.data.color)
+                            .frame(width: 40, height: 40)
+                            .offset(x: offsetWidth, y: self.isDetectingLongPress || self.moving ? -37 : 0)
+                            .gesture(press)
+                            .gesture(drag)
+                            .animation(Animation.spring(dampingFraction: 0.76).speed(1.6))
+                })
+        }
+            .frame(height: 40)
+        
+    }
+}
+
+@available(iOS 13.0, macOS 10.15, *)
+private struct Thumb: View {
+    var label: String = ""
+    
+    var isUp: Bool
+    
+    var color: Color
+
+    
+    var body: some View {
+        
+        ZStack {
+            Path { p in
+                p.move(to: CGPoint(x: 10, y: 38))
+                p.addCurve(to: CGPoint(x: 20, y: 0), control1: CGPoint(x: -2, y: 38), control2: CGPoint(x: -10, y: 2))
+                p.addCurve(to: CGPoint(x: 30, y: 38), control1: CGPoint(x: 50, y: 2), control2: CGPoint(x: 42, y: 38))
+            }
+            .fill(color)
+            Circle()
+                .fill(Color.white)
+                .padding(3)
+            Text(label)
+                .foregroundColor(color)
+                .font(.system(size: 14, design: .monospaced))
+
+            .minimumScaleFactor(0.01)
+
+        }
+        
+
+    }
+}
